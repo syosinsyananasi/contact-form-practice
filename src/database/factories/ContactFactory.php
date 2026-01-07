a6e922d3b6877b15a081334f3b368fd349921a61
nanashi <cibicco323@gmail.com>
2026-01-07 14:25:30 +0900

    factory migrations seeders

---
+++ src/database/factories/ContactFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Contact;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    protected $model = Contact::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => Category::inRandomOrder()->first()->id ?? 1,
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'gender' => $this->faker->numberBetween(1, 3),
+            'email' => $this->faker->unique()->safeEmail(),
+            'tel' => $this->faker->numerify('###########'),
+            'address' => $this->faker->address(),
+            'building' => $this->faker->optional()->secondaryAddress(),
+            'detail' => $this->faker->realText(100),
+        ];
+    }
+}
